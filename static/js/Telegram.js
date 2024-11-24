29c62fd15e6edb9298378fdaa003ba937e779d74
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-11-24 22:56:27 +0300

    Add files via upload

--- static/js/Telegram.js
+++ static/js/Telegram.js
@@ -1,5 +1,8 @@
 var WebApp = window.Telegram.WebApp;
 var username = WebApp.initDataUnsafe.user.username;
+var start_param = WebApp.WebAppInitData.start_param;
+
+alert(`Start param: ${start_param}`)
 
 if (username == undefined) 
     username = `id${WebApp.initDataUnsafe.user.id}`
