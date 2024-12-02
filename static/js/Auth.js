0cd9bef3726b84e38cf9c15bd70de49ff88d5511
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-12-02 21:16:48 +0300

    Add files via upload

--- static/js/Auth.js
+++ static/js/Auth.js
@@ -3,10 +3,11 @@ var WebApp = window.Telegram.WebApp;
 var user_id = WebApp.initDataUnsafe.user.id // user id
 var refer_id = WebApp.initDataUnsafe.start_param; // refer's id
 
+
 httpRequest = new XMLHttpRequest();
 
 httpRequest.open('POST', 'auth');
 var data = JSON.stringify({"user_id": user_id, "refer_id": refer_id});
 httpRequest.send(data);
 
-alert(user_id, refer_id)
+alert(`${user_id}, ${refer_id}`)
